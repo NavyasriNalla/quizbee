fcbf31b6b2d423e9930049611ade378289a5de01
NavyasriNalla <navyasri.nalla27@gmail.com>
2020-07-24 18:45:59 +0530

    Updates

---
+++ precache-manifest.fcc2644417611a8240cedb53fe533412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595733d129bd90453b49fde859a371f4",
+    "url": "/quizbee1/index.html"
+  },
+  {
+    "revision": "8b66625def76a42ca7f0",
+    "url": "/quizbee1/static/css/main.ff070a17.chunk.css"
+  },
+  {
+    "revision": "4bbf9a5d21cbcd9bd0f9",
+    "url": "/quizbee1/static/js/2.7f5fc408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizbee1/static/js/2.7f5fc408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b66625def76a42ca7f0",
+    "url": "/quizbee1/static/js/main.820df3ee.chunk.js"
+  },
+  {
+    "revision": "ce2471f06b2969e2e006",
+    "url": "/quizbee1/static/js/runtime-main.c4060c5e.js"
+  }
+]);
